1ef01e88b2e1ed571c499b83cb96f543efbeeb32
Nidhisood <soodnidhi@seasiainfotech.com>
2017-01-04 11:06:32 +0530

    klj

--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -1,17 +1,20 @@
 ﻿using System;
-		
+
 using UIKit;
 using System.Net;
 
 namespace SQLiteSample.iOS
 {
-	public partial class ViewController : UIViewController,IResultInterface
+	public partial class ViewController : UIViewController, IResultInterface
 	{
-		
+
 
 		public ViewController (IntPtr handle) : base (handle)
-		{		
-			
+		{
+			/// <summary>
+			/// Views the did load.
+			/// </summary>
+			/// <returns>The did load.</returns>
 		}
 
 		public override void ViewDidLoad ()
@@ -25,37 +28,37 @@ namespace SQLiteSample.iOS
 
 			Button.TouchUpInside += delegate {
 
-//				var getChangeApi = new ChangeControlApi (this);
-//				getChangeApi.ChangeControl ("15-Apr-2016");
+				//				var getChangeApi = new ChangeControlApi (this);
+				//				getChangeApi.ChangeControl ("15-Apr-2016");
 
-//				var backgroundTask = new Thread (new ThreadStart (() => {
-//					//RestClient client = new RestClient ("http://teamsstudentapp.rtosoftware.com.au/TeamsStudentApp/ChangeControl");
-//					RestClient client = new RestClient ("http://teamsstudentapp.rtosoftware.com.au/TeamsStudentApp/Campuses");
-//					RestRequest request = new RestRequest (Method.GET);
-//
-//					//request.AddHeader ("SearchDate", "15-Apr-2016");
-//
-//					//GetServiceResponseData response = (GetServiceResponseData)client.Execute (request);
-//
-//					IRestResponse<GetServiceResponseData> response = client.Execute<GetServiceResponseData> (request);
-//
-//					var jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject<List <GetServiceResponseData>> (response.Content);
-//
-//					//RestSharp.Deserializers.
-//
-//					foreach (var item in jsonObj) {
-//
-//						Console.WriteLine (item.CampusAddress + "  " + item.CampusName);
-//						
-//					}
-//				}));
-//				backgroundTask.Start ();
+				//				var backgroundTask = new Thread (new ThreadStart (() => {
+				//					//RestClient client = new RestClient ("http://teamsstudentapp.rtosoftware.com.au/TeamsStudentApp/ChangeControl");
+				//					RestClient client = new RestClient ("http://teamsstudentapp.rtosoftware.com.au/TeamsStudentApp/Campuses");
+				//					RestRequest request = new RestRequest (Method.GET);
+				//
+				//					//request.AddHeader ("SearchDate", "15-Apr-2016");
+				//
+				//					//GetServiceResponseData response = (GetServiceResponseData)client.Execute (request);
+				//
+				//					IRestResponse<GetServiceResponseData> response = client.Execute<GetServiceResponseData> (request);
+				//
+				//					var jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject<List <GetServiceResponseData>> (response.Content);
+				//
+				//					//RestSharp.Deserializers.
+				//
+				//					foreach (var item in jsonObj) {
+				//
+				//						Console.WriteLine (item.CampusAddress + "  " + item.CampusName);
+				//						
+				//					}
+				//				}));
+				//				backgroundTask.Start ();
 
 
 				database.UpdateData ();
 
 			};
-		
+
 		}
 
 		#region IResultInterface implementation
